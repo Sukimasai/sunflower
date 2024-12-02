623de121101ec00fab66ded5c87a099697c07951
Sukimasai <164629287+Sukimasai@users.noreply.github.com>
2024-12-02 20:49:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var himawarinoyakusoku = document.getElementById("sunflowersong");
+    var playpause = true;
+
+    function toggleplay(){
+        if (playpause) {
+            himawarinoyakusoku.pause();
+        } else {
+            himawarinoyakusoku.play();
+        }
+
+        himawarinoyakusoku.onplay = function(){
+            playpause = true;
+        }
+
+        himawarinoyakusoku.onpause = function(){
+            playpause = false;
+        }
+    }
+
+    document.getElementById("sunflowerbutton").onclick = function() {
+        document.getElementById("sunflowerimage").classList.toggle("pause");
+        toggleplay();
+    }
+
+    document.getElementById("playbutton").onclick = function(){
+        himawarinoyakusoku.play();
+    }
+
+    document.getElementById("pausebutton").onclick = function(){
+        himawarinoyakusoku.pause();
+    }
+});
