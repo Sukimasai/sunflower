5fb38011978623c6712b860e54f1c6fc1ac0260e
Sukimasai <164629287+Sukimasai@users.noreply.github.com>
2025-01-08 23:58:30 +0700

    Update script.js
    Optimization

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var himawarinoyakusoku = document.getElementById("sunflowersong");
-    var playpause = false;
+var himawarinoyakusoku = document.getElementById("sunflowersong");
+var playpause = false;
 
-    function toggleplay(){
-        if (playpause) {
-            himawarinoyakusoku.pause();
-        } else {
-            himawarinoyakusoku.play();
-        }
-
-        himawarinoyakusoku.onplay = function(){
-            playpause = true;
-        }
-
-        himawarinoyakusoku.onpause = function(){
-            playpause = false;
-        }
-    }
-
-    document.getElementById("sunflowerbutton").onclick = function() {
-        document.getElementById("sunflowerimage").classList.add("sunfloweranimation");
-        document.getElementById("sunflowerimage").classList.toggle("pause");
-        toggleplay();
-    }
-
-    document.getElementById("playbutton").onclick = function(){
+function toggleplay(){
+    if (playpause) {
+        himawarinoyakusoku.pause();
+    } else {
         himawarinoyakusoku.play();
     }
 
-    document.getElementById("pausebutton").onclick = function(){
-        himawarinoyakusoku.pause();
+    himawarinoyakusoku.onplay = function(){
+        playpause = true;
     }
-});
+
+    himawarinoyakusoku.onpause = function(){
+        playpause = false;
+    }
+}
+
+document.getElementById("sunflowerbutton").onclick = function() {
+    document.getElementById("sunflowerimage").classList.add("sunfloweranimation");
+    document.getElementById("sunflowerimage").classList.toggle("pause");
+    toggleplay();
+}
+
+document.getElementById("playbutton").onclick = function(){
+    himawarinoyakusoku.play();
+}
+
+document.getElementById("pausebutton").onclick = function(){
+    himawarinoyakusoku.pause();
+}
