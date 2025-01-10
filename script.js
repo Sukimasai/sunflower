605befd6881098f6421db75e14b468762464d5fb
Sukimasai <164629287+Sukimasai@users.noreply.github.com>
2025-01-10 22:33:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function toggleplay(){
 }
 
 document.getElementById("sunflowerbutton").onclick = function() {
-    document.getElementById("sunflowerimage").classList.toggle("sunfloweranimation");
+    document.getElementById("sunflowerimage").classList.toggle("pause");
     toggleplay();
 }
 
