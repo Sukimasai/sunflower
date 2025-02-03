3322ead8b78a77531472e30729a48244bd858f5a
Sukimasai <164629287+Sukimasai@users.noreply.github.com>
2025-02-03 23:44:41 +0700

    Update script.js
    removed unused variable

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 var himawarinoyakusoku = document.getElementById("sunflowersong");
-var playpause = false;
 
 function toggleplay() {
     if (himawarinoyakusoku.paused) {
         himawarinoyakusoku.play();
         document.getElementById("sunflowerimage").classList.remove("pause");
-        playpause = true;
     } else {
         himawarinoyakusoku.pause();
         document.getElementById("sunflowerimage").classList.add("pause");
-        playpause = false;
     }
 }
 
@@ -20,11 +17,9 @@ document.getElementById("sunflowerbutton").onclick = function() {
 document.getElementById("playbutton").onclick = function() {
     himawarinoyakusoku.play();
     document.getElementById("sunflowerimage").classList.remove("pause");
-    playpause = true;
 };
 
 document.getElementById("pausebutton").onclick = function() {
     himawarinoyakusoku.pause();
     document.getElementById("sunflowerimage").classList.add("pause");
-    playpause = false;
 };
